73f1f87776c8b6088b7f4c1ec5aa9fa86b94ee8f
Jon Gray <jgray@spartaglobal.com>
2018-11-28 11:43:55 +0000

    confirmed first test working for single postcode

--- lib/services/single_postcode_service.rb
+++ lib/services/single_postcode_service.rb
@@ -7,7 +7,11 @@ class SinglePostcodeService
   base_uri 'https://api.postcodes.io'
 
   def get_single_postcode(postcode)
-    
+    @single_postcode_data = JSON.parse(self.class.get("/postcodes/#{postcode}").body)
+  end
+
+  def get_status_code
+    @single_postcode_data['status']
   end
 
 end
