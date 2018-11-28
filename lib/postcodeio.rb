721bde5605e4df7d25af492d7fe5f41264c40afb
Jon Gray <jgray@spartaglobal.com>
2018-11-28 10:46:08 +0000

    Initial file creation and empty service object model created

---
+++ lib/postcodeio.rb
@@ -0,0 +1,17 @@
+# include in the services
+require_relative 'services/single_postcode_service'
+require_relative 'services/multiple_postcodes_service'
+
+
+# superclass
+class Postcodesio
+
+  def single_postcode_service
+
+  end
+
+  def multiple_postcodes_service
+
+  end
+
+end
